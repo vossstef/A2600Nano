443717a80cb173cfa36ac3d7547f79c3df57c7dd
Stefan Voss <75468830+vossstef@users.noreply.github.com>
2025-07-17 18:53:35 +0200

    onboard bl616 (#46)

--- build_tc60k_bl616.tcl
+++ build_tc60k_bl616.tcl
@@ -37,16 +37,16 @@ add_file src/TIA/TIA.vhd
 add_file src/TIA/VGAColorTable.vhd
 add_file src/TIA/audio_argh2600.vhd
 add_file src/gowin_sdpb/gowin_sdpb_gw5a.vhd
-add_file src/tang/console60k/a2600_top_tc60k.vhd
-add_file src/tang/console60k/a2600_top_tc60k.cst
-add_file src/tang/console60k/a2600_top_tc60k.sdc
+add_file src/tang/console60k_bl616/a2600_top_tc60k.vhd
+add_file src/tang/console60k_bl616/a2600_top_tc60k.cst
+add_file src/tang/console60k_bl616/a2600_top_tc60k.sdc
 add_file src/video_stabilize.sv
 add_file src/gowin_pll/gowin_pll_ntsc_60k.vhd
 add_file src/detect2600.sv
 add_file src/dualshock2.v
 
 set_option -synthesis_tool gowinsynthesis
-set_option -output_base_name a2600nano_console60k
+set_option -output_base_name a2600nano_console60k_bl616
 set_option -verilog_std sysv2017
 set_option -vhdl_std vhd2008
 set_option -top_module A2600_top_tc60k
@@ -57,7 +57,7 @@ set_option -use_cpu_as_gpio 1
 set_option -use_ready_as_gpio 1
 set_option -use_sspi_as_gpio 1
 set_option -use_i2c_as_gpio 1
-set_option -use_jtag_as_gpio 0
+set_option -use_jtag_as_gpio 1
 set_option -print_all_synthesis_warning 0
 set_option -show_all_warn 1
 set_option -rw_check_on_ram 0
